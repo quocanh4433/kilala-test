72d5632addc65a819580751092d300f97d506c9e
Quoc Anh <quocanh@192.168.1.8>
2024-06-04 23:44:58 +0700

    feat-about: coding about

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -6,3 +6,35 @@ function toggleMenu() {
     overlay.classList.toggle('overlay--active')
     nav.classList.toggle('nav--active')
 }
+
+
+
+
+const tabs = document.querySelectorAll('.tab');
+const contents = document.querySelectorAll('.content');
+const underline = document.querySelector('.underline');
+
+function updateUnderline() {
+  const activeTab = document.querySelector('.tab.active');
+  underline.style.width = `${activeTab.offsetWidth + 3}px`;
+  underline.style.left = `${activeTab.offsetLeft - 2}px`;
+}
+
+tabs.forEach(tab => {
+  tab.addEventListener('click', () => {
+    tabs.forEach(t => t.classList.remove('active'));
+    tab.classList.add('active');
+    const target = tab.getAttribute('data-target');
+    contents.forEach(content => {
+      if (content.id === target) {
+        content.classList.add('active');
+      } else {
+        content.classList.remove('active');
+      }
+    });
+    updateUnderline();
+  });
+});
+
+window.addEventListener('resize', updateUnderline);
+updateUnderline();

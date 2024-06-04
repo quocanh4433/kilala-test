3b59081bd353526415658aa6d1f5e1ed4cef1625
Quoc Anh <quocanh@192.168.1.8>
2024-06-04 21:11:39 +0700

    feat-banner: coding banner

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -5,11 +5,4 @@ function toggleMenu() {
     menu.classList.toggle('hamburger--active');
     overlay.classList.toggle('overlay--active')
     nav.classList.toggle('nav--active')
-
-    // let submenu = document.querySelector('.header__subnav');
-    // let logo = document.querySelector('.header__logo');
-    // let body = document.querySelector('body');
-    // submenu.classList.toggle('subnav-active');
-    // logo.classList.toggle('logo-hidden');   
-    // body.classList.toggle('body-unscroll');   
 }
